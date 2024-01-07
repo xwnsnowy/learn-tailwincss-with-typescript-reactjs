b0435884485a93b8001ae489c20b9ec4b7220e13
Nguyen Tien Thanh <thanhnthe163528@fpt.edu.vn>
2024-01-07 15:22:18 +0700

    up

---
+++ learn-tailwin-css/src/App.tsx
@@ -0,0 +1,11 @@
+import { UserProfile } from "./One/UserProfile";
+
+function App() {
+  return (
+    <div className="w-full h-screen bg-[#E6EFFA] flex items-center justify-center">
+      <UserProfile/>
+    </div>
+  )
+}
+
+export default App;
---
+++ learn-tailwin-css/src/One/UserProfile.tsx
@@ -0,0 +1,80 @@
+export const UserProfile = () => {
+  return (
+    <div className="w-[36rem] rounded-lg bg-white p-12 flex gap-4">
+      <img
+        src="https://placewaifu.com/image/200"
+        className="w-32 rounded-full border-[11px] self-start"
+        alt=""
+      />
+      <div className="space-y-7 text-[#1C2B62]">
+        <div>
+          <h1 className="text-3xl font-bold">David Grant</h1>
+          <h2 className="mt-1">3D Artist</h2>
+        </div>
+        <div className="space-y-4">
+          <p className="flex item-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 inline-block mr-2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+              />
+            </svg>
+            4.7 Rating
+          </p>
+          <p className="flex item-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 inline-block mr-2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
+              />
+            </svg>
+            4.447 Reviews
+          </p>
+          <p className="flex item-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 inline-block mr-2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+              />
+            </svg>
+            478 Students
+          </p>
+        </div>
+        <div>
+          <p>
+            Grant has been making video games for a living for more than 14
+            years as a Designer, Producer, Creative Director, and Executive
+            Producer, creating games for console, mobile, PC and Facebook.
+          </p>
+        </div>
+        <div>
+          <button>Show more</button>
+        </div>
+      </div>
+    </div>
+  );
+};
